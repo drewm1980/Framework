f0c181e359d91b8f3cde69cfc6ff4320c6594336
Nathan Fain <nathan@squimp.com>
2018-12-19 12:29:47 +0100

    make parameters arguments to function

--- SymmThinningCUDA/SymmThinningCUDA/main.cu
+++ SymmThinningCUDA/SymmThinningCUDA/main.cu
@@ -1,5 +1,6 @@
 #include <vector>
 #include <iostream>
+#include <cstdlib>
 
 #include "thinning.h"
 #include "h5_io.h"
@@ -7,16 +8,30 @@
 
 int main(int argc, char *argv[])
 {
-	
-	unsigned width = 256U, height = 256U;
-	unsigned numSlices = 256U;
+
+	if (argc < 4) {
+		std::cout << "arguments: depth directory maxIter p (e.g. 256 oldH5 80 10)" << std::endl;
+		return 1;
+	}
+	unsigned numSlices = std::atoi(argv[1]);
+	unsigned width = numSlices, height = numSlices;
+	unsigned maxIter = std::atoi(argv[3]);
+	unsigned p = std::atoi(argv[4]);
+
+	std::cout << "using dimension:" << std::endl;
+	std::cout << "  numSlices:" << numSlices << std::endl;
+	std::cout << "      width:" << width << std::endl;
+	std::cout << "     height:" << height << std::endl;
+	std::cout << "initial data from:" << std::endl;
+	std::cout << "   " << argv[2] << std::endl;
+	std::cout << "other configuration elements:" << std::endl;
+	std::cout << "    maxIter:" << maxIter << std::endl;
+	std::cout << "          p:" << p << std::endl;
+
 	thin::IjkType size3D = thin::makeIjk(width, height, numSlices);
 	unsigned maxNumVoxelsPerChunk = 100000U;
 
-	h5_io::H5SliceIoManager sliceIoMngr("", "oldH5", "newH5", width, height, numSlices, "chunkMap.txt", maxNumVoxelsPerChunk);
-	
-	unsigned curIter = 0, curDim = 3U, maxIter = 80U;
-	unsigned p = 10U;
+	h5_io::H5SliceIoManager sliceIoMngr("", argv[2], "newH5", width, height, numSlices, "chunkMap.txt", maxNumVoxelsPerChunk);
 
 	thin::initDevice();
 	thin::setNumThreadsPerBlock(196U);
